bf500056306398490297b3c90bca4b0f0208de3a
erickakoyama <ericka.koyama@gmail.com>
2020-11-18 03:27:19 +0000

    wip

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 app.engine('html', require('ejs').renderFile);
+app.use(express.static('public'));
 
 // routes
 app.get('/', (req, res) => {
